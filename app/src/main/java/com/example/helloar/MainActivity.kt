798dab7d6326a432352e5543787717c6e7e5699b
cmorilloUala <constanza.morillo@uala.com.ar>
2021-09-04 17:31:15 -0300

    new

--- app/src/main/java/com/example/helloar/MainActivity.kt
+++ app/src/main/java/com/example/helloar/MainActivity.kt
@@ -17,7 +17,7 @@ import com.google.ar.sceneform.ux.TransformableNode
 
 class MainActivity : AppCompatActivity() {
     private lateinit var arFragment: ArFragment
-    private val modelLink = "https://github.com/cmorilloUala/augmented-reality/raw/master/model/scene.gltf"
+    private val modelLink = "https://github.com/yudiz-solutions/runtime_ar_android/raw/master/model/model.gltf"
     private lateinit var renderable: ModelRenderable
 
     override fun onCreate(savedInstanceState: Bundle?) {
